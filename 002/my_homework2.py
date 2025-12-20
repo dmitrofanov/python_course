694535e5f913560d0621584d1f86a9df2b304bb4
Pavel <freespace1989@gmail.com>
2025-12-21 02:32:30 +0300

    add task 3

--- 002/my_homework2.py
+++ 002/my_homework2.py
@@ -1,9 +1,9 @@
 ## 1. Написать программу которая выводит таблицу умножения для заданного числа
 
-# number = int(input('Введите число для таблицы умножения: '))
-# for i in range(1,10):
-#     result = number * i
-#     print(f"{number} * {i} = {result}")
+number = int(input('Введите число для таблицы умножения: '))
+for i in range(1,10):
+    result = number * i
+    print(f"{number} * {i} = {result}")
 
 # 2. «Умный анализатор списка»
 #     Запрашивает у пользователя числа через запятую, преобразует в список.
@@ -13,44 +13,56 @@
 #         Количество чётных чисел (через if и %).
 #     Усложнение: Вывести числа, которые больше среднего арифметического всего списка.
 
-# numbers = input('Введите числа через запятую : ')
-# numbers_list = numbers.split(',')
-# total = 0
-# for number in numbers_list:
-#     total += int(number)
-# print(f'Сумма чисел: {total}')
+numbers = input('Введите числа через запятую : ')
+numbers_list = numbers.split(',')
+total = 0
+for number in numbers_list:
+    total += int(number)
+print(f'Сумма чисел: {total}')
 
 
-# x = float('-inf')
-# for number in numbers_list:
-#     if int(number) > x:
-#         x = int(number)
-# print(f'Максимальное число: {x}')
-# for number in numbers_list:
-#     if int(number) < x:
-#         x = int(number)
-# print(f'Минимальное число: {x}')
+x = float('-inf')
+for number in numbers_list:
+    if int(number) > x:
+        x = int(number)
+print(f'Максимальное число: {x}')
+for number in numbers_list:
+    if int(number) < x:
+        x = int(number)
+print(f'Минимальное число: {x}')
 
-# even_number = 0
-# for number in numbers_list:
-#     if int(number) %2 == 0:
-#         even_number += 1
-# print(f'Количество четных чисел : {even_number}')
+even_number = 0
+for number in numbers_list:
+    if int(number) %2 == 0:
+        even_number += 1
+print(f'Количество четных чисел : {even_number}')
 
 
-# count = 0
-# for number in numbers_list:
-#     count += 1
-# average_sum = total / count
-# z = 0
-# for number in numbers_list:
-#     if int(number) > average_sum:
-#         z = int(number)
-# print(f'Число больше среднего арифмитического: {z}')
+count = 0
+for number in numbers_list:
+    count += 1
+average_sum = total / count
+z = 0
+for number in numbers_list:
+    if int(number) > average_sum:
+        z = int(number)
+print(f'Число больше среднего арифмитического: {z}')
 
 
 # 3. «Поисковик дубликатов»
 #     Дан список: items = ["яблоко", "банан", "киви", "яблоко", "апельсин", "банан", "яблоко"].
 #     С помощью вложенных циклов for (или for и оператора in) найти и вывести все элементы, которые встречаются больше одного раза, но каждый дубликат вывести только один раз (например, "яблоко, банан").
 
-items = ["яблоко", "банан", "киви", "яблоко", "апельсин", "банан", "яблоко"]
+items = ["яблоко", "банан", "киви", "яблоко", "апельсин", "банан", "яблоко", "апельсин"]
+list_new = []
+for x in items:
+    if items.count(x) > 1:
+        if x not in list_new:
+            list_new.append(x)
+print(list_new)
+
+# 4. «Шифратор цезаря (базовый)»
+#     Запросить у пользователя строку и шаг сдвига n.
+#     Создать новую строку, где каждая буква из исходной сдвинута в алфавите на n позиций. Пока только для маленьких букв и без учёта конца алфавита.
+#     Использовать: цикл for по строке, ord() для получения кода буквы, chr() для обратного преобразования, if для проверки, что это буква (char.isascii()).
+
