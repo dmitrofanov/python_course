5f9fe570dfddac706caa81980f8fd0ca6fca41ea
Pavel <freespace1989@gmail.com>
2025-12-25 01:09:35 +0300

    add hw3

--- 003/my_homework/hw_3.py
+++ 003/my_homework/hw_3.py
@@ -1,1 +1,6 @@
 #Создать функцию, которая принимает строку и печатает каждое слово с новой строки. Использовать функции, созданные в заданиях 1 и 2.
+def list_of_strings(text):
+    words = text.split()
+    for word in words:
+        print(word)
+list_of_strings('h fd gf 333')
