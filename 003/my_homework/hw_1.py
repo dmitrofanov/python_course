5e852b4a4b6823401a68910fed6f53aa47b36306
Pavel <freespace1989@gmail.com>
2025-12-25 00:34:45 +0300

    add 3 hw

---
+++ 003/my_homework/hw_1.py
@@ -0,0 +1,5 @@
+#Создать функцию, которая принимает список слов и печатает каждое слово с новой строки.
+def list_of_words(words):
+    for word in words:
+        print(word)
+list_of_words(['Pavel','Privet',64])
