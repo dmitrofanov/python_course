ae2084bdb67099912c1b789f7bfe7cec597bacd6
Pavel <freespace1989@gmail.com>
2026-01-26 23:55:44 +0300

    all

--- 006/homework/hw_func.py
+++ 006/homework/hw_func.py
@@ -38,3 +38,7 @@ def countdown(number):
 countdown(5)
 
 # 5) Напиши функцию words, которая принимает предложение (строку) и возвращает список слов в этом предложении.
+
+def words(text):
+    return text.split()
+print(words('slovo1 slovo2 slovo3'))
