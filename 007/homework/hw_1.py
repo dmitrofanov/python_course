adad6217c880f3c5934006484259893d318c65a8
Pavel <freespace1989@gmail.com>
2026-01-29 21:52:17 +0300

    add hw_1 v.1.0

--- 007/homework/hw_1.py
+++ 007/homework/hw_1.py
@@ -13,3 +13,25 @@
 
 Выполни в одну строку: sum_all(square_all(filter_even(исходный_список)))
 """
+
+def filter_even(numbers):
+    even_numbers = list()
+    for even_number in numbers:
+        if even_number % 2 == 0:
+            even_numbers.append(even_number)
+    return even_numbers
+
+def square_all(filter_even):
+    square_numbers = list()
+    for square_number in filter_even:
+        square_numbers.append(square_number ** 2)
+    return square_numbers
+
+def sum_all(square_all):
+    total = 0
+    for sum_number in square_all:
+        total = total + sum_number
+    return total
+
+print(sum_all(square_all(filter_even([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))))
+          
