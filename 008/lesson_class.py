38a42a25518b6e02b03d9a4e516a35ff0a76ab9e
Dmitry Mitrofanov <mail@dmitrof.com>
2026-02-01 21:03:45 +0300

    Lesson 8 classes

--- 008/lesson_class.py
+++ 008/lesson_class.py
@@ -1,14 +1,27 @@
+import requests
+
+servers = [
+    '192.168.10.43',
+    '192.168.10.44',
+    '192.168.10.45',
+    '192.168.10.46',
+]
+
 class Server:
-    def __init__(self, login, password, api_url):
+    def __init__(self, ip, login, password):
+        self.ip = ip
         self.login = login
         self.password = password
-        self.api_url = api_url
-    
+        print(f'Initialized {ip}')
+
+    def get_properties(self):
+        print(f'Connecting to {self.ip} using {self.login}/{self.password}')
+        response = requests.get('https://api.restful-api.dev/objects')
+        item = response.json()[1]
+        return dict(model=item['name'], serial=item['id'])
 
 
 
-host1 = Server('login', 'password')
-print(host1.login)
-
-# 'https://api.restful-api.dev/objects'
-# requests status_code, json()
+for ip in servers:
+    server = Server(ip, 'SUPER_LOGIN', 'SUPER_PASSWORD')
+    print(server.get_properties())
