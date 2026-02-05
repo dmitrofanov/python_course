2cdcb1c98090100cb944067c575e1ffd117b1c00
Pavel <freespace1989@gmail.com>
2026-02-05 22:23:45 +0300

    add hw 9 lesson

--- 009/homework/hw_1.py
+++ 009/homework/hw_1.py
@@ -9,3 +9,32 @@
 
 Создай студента, добавь оценки 5, 4, 5, выведи информацию.
 """
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.grades = []
+    
+    def add_grade(self, grade):
+        self.grades.append(grade)
+        
+    def get_average(self):
+        if len(self.grades) > 0:
+            return sum(self.grades) / len(self.grades)
+        else:
+            return 0
+    
+    def info(self):
+        print(f'Имя: {self.name} Возраст: {self.age} Средний балл: {self.get_average()} ')
+
+student1 = Student("Павел", 36)
+student1.add_grade(5)
+student1.add_grade(4)
+student1.add_grade(5)
+student1.info()
+
+
+
+
+
--- 009/homework/hw_2.py
+++ 009/homework/hw_2.py
@@ -9,3 +9,27 @@
 
 Создай прямоугольник 5x3, вычисли площадь и периметр, увеличь в 2 раза.
 """
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
+    def area(self):
+        return self.width * self.height
+    
+    def perimeter(self):
+        return (2 *(self.width + self.height))
+    
+    def scale(self, factor):
+        self.width =self.width * factor
+        self.height = self.height * factor
+    
+    def info(self):
+        print(f'Ширина: {self.width} Высота: {self.height} Периметр: {self.perimeter()} Площадь: {self.area()}')
+
+rectangle = Rectangle(5, 3)
+rectangle.area()
+rectangle.perimeter()
+rectangle.scale(2)
+rectangle.info()
