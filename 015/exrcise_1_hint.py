99d81594e191202ece28d4a5e2897986bd1a4ae1
Dmitry Mitrofanov <mail@dmitrof.com>
2026-03-04 19:03:10 +0300

    Lesson 15

---
+++ 015/exrcise_1_hint.py
@@ -0,0 +1,16 @@
+n = len(numbers)
+
+for i in range(n - 1):
+    # Ищем минимум в оставшейся части
+    min_index = i
+    for j in range(i + 1, n):
+        if numbers[j] < numbers[min_index]:
+            min_index = j
+    
+    # Меняем местами
+    if min_index != i:
+        numbers[i], numbers[min_index] = numbers[min_index], numbers[i]
+    
+    print(f"Шаг {i+1}: {numbers}")
+
+print("\nОтсортированный список:", numbers)
