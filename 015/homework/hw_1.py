f83a51953f7c4fb4391f392a0b7fe958af37d013
Dmitry Mitrofanov <mail@dmitrof.com>
2026-03-05 21:10:56 +0300

    Homework 15

---
+++ 015/homework/hw_1.py
@@ -0,0 +1,32 @@
+"""
+ЗАДАЧА 9: Валидация конфигурационного файла
+
+DevOps часто проверяет конфиги перед деплоем.
+Проверь, что в конфиге есть все обязательные поля.
+"""
+config = {
+    "database": {
+        "host": "localhost",
+        "port": 5432,
+        # "username" отсутствует!
+        "password": "secret"
+    },
+    "server": {
+        "port": 8080
+        # "workers" отсутствует!
+    }
+}
+
+required_fields = [
+    ("database", "host"),
+    ("database", "port"),
+    ("database", "username"),
+    ("database", "password"),
+    ("server", "port"),
+    ("server", "workers"),
+]
+
+# Твой алгоритм:
+# 1. Пройди по всем обязательным полям
+# 2. Проверь, что они есть в конфиге
+# 3. Собери список отсутствующих
