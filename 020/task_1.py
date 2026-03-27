ef00c864c3fcf34dd9667dc2d7f2154d1be95410
Pavel <freespace1989@gmail.com>
2026-03-28 00:17:39 +0300

    add hw and last lesson

--- 020/task_1.py
+++ 020/task_1.py
@@ -14,11 +14,35 @@
 """
 
 cart = [
-    {"name": "Футболка", "price": 1000, "quantity": 2},
-    {"name": "Джинсы", "price": 2500, "quantity": 1},
+    {"name": "Футболка", "price": 1000, "quantity": 20},
+    {"name": "Джинсы", "price": 2500, "quantity": 2},
     {"name": "Кепка", "price": 800, "quantity": 3},
     {"name": "Кроссовки", "price": 3500, "quantity": 1},
     {"name": "Носки", "price": 200, "quantity": 5},
 ]
 
 # Решение
+# for product in cart:
+#     print(f"товар: {product["price"]} стоимость: {product["price"] * product["quantity"]}")
+
+print([print(f"товар: {product["name"]} стоимость: {product["price"] * product["quantity"]}") for product in cart])
+
+print(f'общая стоимость корзины: {sum([(product["price"] * product["quantity"]) for product in cart])}')
+
+# max_price = 0
+# max_price_product = None
+# for product in cart:
+#     if product["price"] > max_price:
+#         max_price_product = product["name"]
+#         max_price = product["price"]
+# print(f'Товар с мах стоимостью: {max_price_product}')
+    
+def get_price(product):
+    return product["price"]
+
+
+print(f'Товар с мах стоимостью: {max(cart, key = get_price )["name"]}')  #или вместо key = lambda x : x["price"]
+
+print(f'Товар с максимальной итоговой стоимостью: {max(cart, key = lambda x : x["price"] * x["quantity"]) ["name"]}')
+
+
