773ab696de46d689a16515df419aac2f3543df26
Dmitry Mitrofanov <mail@dmitrof.com>
2026-04-01 20:10:59 +0300

    Lesson 21

--- 21/interview.py
+++ 21/interview.py
@@ -1,11 +1,11 @@
 students = {}
 students['students'] = [
-    {'name': 'Анна', 'age': 20, 'courses': {
+    {'name': 'Анна', 'age': 20, 'courses': [
         {'name': 'Math', 'grade': 5},
         {'name': 'English', 'grade': 4}
-    }},
-    {'name': 'Пётр', 'age': 25, 'courses': {
+    ]},
+    {'name': 'Пётр', 'age': 25, 'courses': [
         {'name': 'Math', 'grade': 3},
         {'name': 'English', 'grade': 5}
-    }}
+    ]}
 ]
