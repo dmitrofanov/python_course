2d72232830890dae18a5477e9f07d7a4a4573434
Pavel <freespace1989@gmail.com>
2026-04-25 23:55:29 +0300

    add hw3

--- 022/homework/letter_count.py
+++ 022/homework/letter_count.py
@@ -4,3 +4,17 @@
 """
 
 word = "программирование"
+
+
+from collections import Counter
+c = Counter(word)
+print(dict(c))
+
+
+count_dict = {}
+for letter in word:
+    if letter in count_dict:
+        count_dict[letter] += 1
+    else:
+        count_dict[letter] = 1
+print(count_dict)
