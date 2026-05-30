725f6f33d09350681027bd3f885ea0cc4d7bdbb1
Pavel <freespace1989@gmail.com>
2026-05-31 01:15:02 +0300

    add hw likes

--- 025/homework/medium/likes.py
+++ 025/homework/medium/likes.py
@@ -1,6 +1,7 @@
 """
 У тебя есть словарь, где ключ — пост, значение — множество поставивших лайк.
 Для каждого поста выведи количество лайков и список имён.
+Для каждого человека вывести посты,для которых он поставил лайк
 """
 likes = {
     "post_1": {"Анна", "Иван", "Мария"},
@@ -16,4 +17,13 @@ for post, users in likes.items():
     else:
         names = "лайков нет"
 
-    print(f"{post} :{likes_count} - {names}")
+    print(f"{post} : {likes_count} - {names}")
+
+
+result = {}
+for post, users in likes.items():
+    for user in users:
+        if user not in result:
+            result[user] = []
+        result[user].append(post)
+print(result)
