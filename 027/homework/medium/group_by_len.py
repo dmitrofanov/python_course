442d726f4736ec0761e8e165194cba5e5387f38c
Pavel <freespace1989@gmail.com>
2026-06-14 23:16:35 +0300

    add hw lesso 27

--- 027/homework/medium/group_by_len.py
+++ 027/homework/medium/group_by_len.py
@@ -3,3 +3,12 @@
 ключ — длина, значение — список слов этой длины.
 """
 words = ["кот", "собака", "слон", "бегемот", "носорог", "лис", "волк", "ёж"]
+
+sorted_words = {}
+
+for word in words:
+    length_word= len(word)
+    if length_word not in sorted_words:
+        sorted_words[length_word] = []
+    sorted_words[length_word].append(word)
+print(sorted_words)
