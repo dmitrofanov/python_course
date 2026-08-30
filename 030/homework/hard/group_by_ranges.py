01b022fbb2c35e8e15874d2493e6f4453709c16e
Pavel <freespace1989@gmail.com>
2026-08-31 00:48:03 +0300

    add part of hard hw

--- 030/homework/hard/group_by_ranges.py
+++ 030/homework/hard/group_by_ranges.py
@@ -4,4 +4,30 @@
 Диапазоны: 0-9, 10-19, 20-29, 30-39, 40-49, 50+
 """
 
-numbers = [5, 12, 23, 8, 35, 42, 15, 28, 7, 33, 45, 18, 52, 9, 37, 48, 55]
+numbers = [5, 12, 23, 8, 35, 42, 15, 28, 7, 33, 45, 18, 52, 9, 37, 48, 55, 89]
+
+ranges = {
+    "0-9": [],
+    "10-19": [],
+    "20-29": [],
+    "30-39": [],
+    "40-49": [],
+    "50+": []
+}
+
+for num in numbers:
+    if num < 10:
+        ranges["0-9"].append(num)
+    elif num < 20:
+        ranges["10-19"].append(num)
+    elif num < 30:
+        ranges["20-29"].append(num)
+    elif num < 40:
+        ranges["30-39"].append(num)
+    elif num < 50:
+        ranges["40-49"].append(num)
+    else:
+        ranges["50+"].append(num)
+
+for key, value in ranges.items():
+    print(key, value)
--- 030/homework/hard/invert_dict.py
+++ 030/homework/hard/invert_dict.py
@@ -7,3 +7,16 @@
 original = {"a": 1, "b": 2, "c": 1, "d": 3, "e": 2, "f": 1, "g": 4}
 
 inverted = {}
+for key, value in original.items():
+    if value not in inverted:
+        inverted[value] = []
+    inverted[value].append(key)
+
+result = {}
+for value, keys in inverted.items():
+    if len(keys) == 1:
+        result[value] = keys[0]
+    else:
+        result[value] = keys
+
+print(result)
